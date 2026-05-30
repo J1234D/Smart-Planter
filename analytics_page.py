8426c630582b183758ff4680f4f1abc337c3fa1b
J1234D <100943748+J1234D@users.noreply.github.com>
2026-05-30 19:40:57 +0530

    Update analytics_page.py

--- analytics_page.py
+++ analytics_page.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import requests
 
 def AnalyticsPage():
 
     st.title("Analytics")
+    response = requests.get("https://apiforsmartplanter.onrender.com/history")
+    data = response.json()
+    st.dataframe(data)
