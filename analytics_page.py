f1cbef8016b4ab90e6d591e30af67c21d25a687c
J1234D <100943748+J1234D@users.noreply.github.com>
2026-05-31 08:47:53 +0530

    Update analytics_page.py

--- analytics_page.py
+++ analytics_page.py
@@ -4,7 +4,7 @@ import requests
 def AnalyticsPage():
 
     st.title("Analytics")
-    st.subheader("Work is not completed for this page.")
+    st.text("Work is not completed for this page.")
     response = requests.get("https://apiforsmartplanter.onrender.com/history")
     data = response.json()
     st.dataframe(data)
