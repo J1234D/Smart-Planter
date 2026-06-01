0de99c0eb23f1096d2fe191ecf96bb963dcc862d
J1234D <100943748+J1234D@users.noreply.github.com>
2026-06-01 08:58:59 +0530

    Update about_page.py

--- about_page.py
+++ about_page.py
@@ -52,7 +52,7 @@ Built with Python, IoT, and a passion for learning.
 
 """)
         with col1:
-            st.image("flowchart.png",width=500)
+            st.image("flowchart.png",width=1000)
 
     st.markdown("""<h2>Designed by Students of </h2>
     <h1 style='text-align: center; color: white;'>
